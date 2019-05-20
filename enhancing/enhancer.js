11b8dd6ca32243a8881ca5b75c43ca6afeae763a
ohheyimjen <jendority@gmail.com>
2019-05-20 15:50:02 -0700

    added base repair test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,15 +2,33 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const newItem = {
+      name: item.name,
+      durability: 100,
+      enhancement: item.enhancement + 1
+    };
+    return newItem;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+    return newItem;
+  } else {
+    return item;
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,30 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed, fail} = require('./enhancer.js');
 // test away!
+describe('the game enhancer', () => {
+
+  // REPAIR TEST
+  describe('repair', () => {
+  // test
+    it('should restore durability to 100', () => {
+      const item = {
+        name: 'Luigi',
+        durability: 75,
+        enhancement: 15
+      };
+
+  // arrange
+      const expected = {
+        name: 'Luigi',
+        durability: 100,
+        enhancement: 15
+      };
+
+  // act
+      const repairedItem = repair(item);
+
+  // assert 
+      expect(repairedItem).toEqual(expected);
+    });
+    
+  })
+})
